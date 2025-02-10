125bea96986a920a84224c2966fca009ea6aa296
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-02-10 14:17:04 -0800

    Main.cpp updated with displayMenu function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Fitness Tracker
+//
+//  Created by Subigya Parajuli on 2/10/25.
+//
+
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include "Utils/fitness.hpp"
+
+
+int main(int argc, const char * argv[]) {
+    
+displayMenu(); //function calling in main - now works! 
+    
+
+}
