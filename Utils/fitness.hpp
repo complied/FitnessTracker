89a8d79bc340bc46685378f05a5c44488e83ee84
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-02-10 14:29:15 -0800

    Fitness.hpp has been updated to define the function displayMenu and included all the necesarry libraries

---
+++ Utils/fitness.hpp
@@ -0,0 +1,11 @@
+#ifndef Utils_fitness
+#define Utils_fitness
+
+// Included all the necesary libraries
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+void displayMenu(); // defined function in the hpp file!
+    
+# endif
