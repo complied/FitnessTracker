e6b5f4f63836ba8e7de51b83cbdf1f065e54d97c
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-02-14 23:55:54 -0800

    Updated with Function Declaration and necessary adjustments

---
+++ Utils/workoutReccomendation.hpp
@@ -0,0 +1,14 @@
+#ifndef Utils_wworkoutReccomendation
+#define Utils_wworkoutReccomendation
+
+// Included all the necesary libraries
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include <string>
+
+void workoutReccomendation(); // defined function in the hpp file!
+
+# endif
+
+
