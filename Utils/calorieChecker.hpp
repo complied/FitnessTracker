fdc5c3779620b15e9e5196d0e0cdf6490d8577ea
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-02-14 23:42:35 -0800

    CalorieChecker.hpp updated with new organization and function has been declared inslide

---
+++ Utils/calorieChecker.hpp
@@ -0,0 +1,13 @@
+#ifndef Utils_calorieChecker
+#define Utils_calorieChecker
+
+// Included all the necesary libraries
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include <string> 
+
+void calorieChecker(); // defined function in the hpp file!
+
+# endif
+
