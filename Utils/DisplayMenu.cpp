0e62218cb35c698828bac5deb6ad2df4c4e9ab11
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-04-30 22:20:45 -0700

    Updated Display Menu for better detail

--- Utils/DisplayMenu.cpp
+++ Utils/DisplayMenu.cpp
@@ -5,16 +5,30 @@ using namespace std;
 
 namespace displayMenu {
 //created a displayMenu function that now shows us the basic menu output!
+
 void displayMenu() {
-    cout << setfill(' ') << setw(10) << "" << setfill(' ') << setw(30) << "" << endl;
-    cout << setw(10) << "" << "Welcome to the fitness tracker " << endl;
-    cout << setfill(' ') << setw(10) << "" << setfill('=') << setw(30) << "" << endl;
-    cout << setfill (' ') << setw(10) << "" << "Out of the following choose one." << endl;
-    cout << setfill(' ') << setw(10) << "" << setfill('=') << setw(30) << "" << endl;
-    cout << setfill(' ') << setw(10) << ""<< "1) Calorie Checker." << endl;
-    cout << setfill(' ') << setw(10) << ""<< "2) Workout Reccomendation." << endl;
-    cout << setfill(' ') << setw(10) << ""<< "3) Logging workout." << endl;
-    cout << setfill(' ') << setw(10) << "" << setfill('-') << setw(5) << "" <<"> ";
+    const int width = 50;
+
+    cout << setfill('=') << setw(width) << "=" << endl;
+    cout << setfill(' ') << setw((width + 28) / 2) << "Welcome to the Fitness Tracker" << endl;
+    cout << setfill('=') << setw(width) << "=" << endl;
+    cout << endl;
+
+    cout << setw((width + 32) / 2) << "Please choose an option below:" << endl;
+    cout << setfill('-') << setw(width) << "-" << endl;
+
+    cout << setfill(' ') << setw(5) << "" << "1) Calorie Checker" << endl;
+    cout << setw(5) << "" << "2) Workout Recommendation" << endl;
+    cout << setw(5) << "" << "3) Log Workout" << endl;
+    cout << setw(5) << "" << "4) Routine Assigner" << endl;
+    cout << setw(5) << "" << "5) Save and Read Workout Data" << endl;
+
+    cout << setfill('-') << setw(width) << "-" << endl;
+    cout << endl;
+    cout << setw((width + 8) / 2) << ">> ";
     }
+
 }
+
+
 // Used Basic Iomanip Libarries to create this!
